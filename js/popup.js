2424b8895e500296372c970583e1ed13c8831a38
Timothy Ng <timnjj@gmail.com>
2017-01-15 12:55:11 +0800

    Added minimal quicksave prompt UI

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,18 @@
+/* INITIALIZATION */
+
+/* EVENT HANDLERS */
 document.getElementById("btn-main").onclick = function() {
     window.open('main.html');
+}
+
+document.getElementById("btn-save").onclick = function() {
+    alertify
+        .defaultValue("My Session")
+        .prompt("Give this cache a name:",
+            function(value, event) { //if 'ok' is selected
+                alertify
+                    .logPosition("top left")
+                    .delay(1500)
+                    .success("Session saved as '" + value + "'");
+            }, null); //do nothing if cancel
 }
