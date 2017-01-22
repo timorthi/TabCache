df40ce39c7adc3f9d78f95342c1c73ac69e4ea6e
Timothy Ng <timnjj@gmail.com>
2017-01-22 14:49:10 -0800

    Modified getCurrentTabs to return a custom Tab object with compressed data

--- js/api.js
+++ js/api.js
@@ -1,12 +1,25 @@
+class Tab {
+    constructor(title, url) {
+        this.title = title;
+        this.url = url;
+    }
+}
+
 function getCurrentTabs(callback) {
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
+        var result = [];
         for(var i=0; i<tabs.length; i++) {
             if(tabs[i].url.indexOf("chrome-extension://") !== -1) {
-                //Remove TabCache pages from array
+            //Remove TabCache pages from array
                 tabs.splice(i, 1);
+            } else {
+            //Add custom Tab object to result array
+                var tb = new Tab(LZString.compress(tabs[i].title),
+                                 LZString.compress(tabs[i].url));
+                result.push(tb);
             }
         }
-        callback(tabs);
+        callback(result);
     });
 }
 
